e6fc224b7a36fff1cfca20b29fec026cb5c48edb
Denis Mescheryakov <denis.m.pcspace@gmail.com>
2023-02-22 19:46:44 +0100

    setup github actions

--- src/example.js
+++ src/example.js
@@ -1,22 +1,22 @@
-function splitsTheStringIntoPairs(item){
-    const array_with_pairs = []
-    let pairs = ''
+function splitsTheStringIntoPairs (item) {
+  const array_with_pairs = []
+  let pairs = ''
 
-    for (let i = 0; i < item.length; i++) {
-        pairs += item[i]
-        if (pairs.length === 2) {
-            array_with_pairs.push(pairs)
-            pairs = ''
-        }
+  for (let i = 0; i < item.length; i++) {
+    pairs += item[i]
+    if (pairs.length === 2) {
+      array_with_pairs.push(pairs)
+      pairs = ''
     }
+  }
 
-    if (pairs) {
-        array_with_pairs.push(pairs + '_')
-    }
+  if (pairs) {
+    array_with_pairs.push(pairs + '_')
+  }
 
-    return array_with_pairs
+  return array_with_pairs
 }
 
 module.exports = {
-    splitsTheStringIntoPairs
+  splitsTheStringIntoPairs
 }
